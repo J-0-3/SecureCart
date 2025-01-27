fe04721d013d1439c0c18f67d2bfb50ca92762f5
Joe <38348883+J-0-3@users.noreply.github.com>
2025-01-27 19:20:28 +0000

    ci: moves linting rules into Cargo.toml

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,14 +1,18 @@
+//! This crate implements the backend API for the `SecureCart` ecommerce platform.
+
 mod constants;
+mod controllers;
 mod db;
 mod routes;
-mod controllers;
 mod utils;
+
+use axum::routing::get;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new()
-        .route("/", axum::routing::get(root));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+    let app = axum::Router::new().route("/", get(root));
+    let listener = TcpListener::bind("0.0.0.0:8080")
         .await
         .expect("Failed to bind listener");
     axum::serve(listener, app)
@@ -16,7 +20,7 @@ async fn main() {
         .expect("Failed to init Axum service");
 }
 
+/// The / route is simply used as an availability check.
 async fn root() -> String {
-    "Auth service is running!".to_string()
+    "API is running!".to_owned()
 }
-
