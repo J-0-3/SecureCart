c5ca40ab99bb4ff0e399558b44ad222814df9662
Joe <38348883+J-0-3@users.noreply.github.com>
2025-01-27 13:59:12 +0000

    style: replaces generic Default::default call with String:default

--- backend/api/src/constants/secrets.rs
+++ backend/api/src/constants/secrets.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 const DOCKER_SECRETS_PATH: &str = "/run/secrets/";
 
 pub fn read_secret(name: &str) -> Result<String, std::io::Error> {
-    let mut secret_val = Default::default();
+    let mut secret_val = String::default();
     File::open(Path::new(DOCKER_SECRETS_PATH).join(name.to_lowercase()))?
         .read_to_string(&mut secret_val)?;
     Ok(secret_val)
