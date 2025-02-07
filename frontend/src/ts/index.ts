213d5a0cedf19f64e1ff9c7c318ac22d81447a13
Joe <38348883+J-0-3@users.noreply.github.com>
2025-02-07 00:20:27 +0000

    fix: prevents form submit from reloading the page, refactor: moves failure modal data into javascript

--- frontend/src/ts/index.ts
+++ frontend/src/ts/index.ts
@@ -1,11 +1,25 @@
+type UserRole = "Customer" | "Administrator";
+
+interface WhoamiResponse {
+    user_id: number,
+    role: UserRole
+}
+
 async function check_auth_and_redirect() {
     const response = await fetch("/api/auth/whoami");
     const redirect_message = document.getElementById("redirect-message")!;
     const redirect_link = <HTMLAnchorElement>document.getElementById("redirect-link")!;
     if (response.status == 200) {
-        redirect_link.href = "/store.html";
-        redirect_message.hidden = false;
-        window.location.replace("/store.html");
+        const body: WhoamiResponse = await response.json();
+        if (body.role == "Administrator") {
+            redirect_link.href = "/admin/dashboard.html";
+            redirect_message.hidden = false;
+            window.location.replace("/admin/dashboard.html");
+        } else {
+            redirect_link.href = "/store.html";
+            redirect_message.hidden = false;
+            window.location.replace("/store.html");
+        }
     } else if (response.status == 401) {
         redirect_link.href = "/login.html";
         redirect_message.hidden = false;
--- frontend/src/ts/login.ts
+++ frontend/src/ts/login.ts
@@ -1,7 +1,16 @@
+interface AuthenticationResponse {
+    mfa_required: boolean,
+    is_admin?: boolean
+}
+
+function show_failure_modal(message: string) {
+    document.getElementById("failure-modal-body")!.textContent = message;
+    const modal = new bootstrap.Modal(document.getElementById("failure-modal")!);
+    modal.show()
+}
 async function attempt_login() {
-    const email = (<HTMLInputElement>document.getElementById("email")).value;
-    const password = (<HTMLInputElement>document.getElementById("password")).value;
-    const failure_modal = new bootstrap.Modal(document.getElementById('loginFailedModal')!, {});
+    const email = (document.getElementById("email")! as HTMLInputElement).value;
+    const password = (document.getElementById("password")! as HTMLInputElement).value;
     const response = await fetch("/api/auth/login", {
         method: "post",
         headers: { "Content-Type": "application/json" },
@@ -14,23 +23,28 @@ async function attempt_login() {
             }
         })
     });
-    const body = await response.json();
-    switch (response.status) {
-        case 401:
-            failure_modal.show();
-            break;
-        case 200:
-            if (body.mfa_required) {
-                alert("Redirect to MFA");
-            } else {
-                window.location.replace("/");
-            }
-            break
+    if (response.status === 200) {
+        const body: AuthenticationResponse = await response.json();
+        if (body.mfa_required) {
+            alert("Todo: Add a 2FA frontend here (for testing POST to /api/auth/2fa)");
+        } else {
+            window.location.replace("/");
+        }
+    } else if (response.status === 401) {
+        show_failure_modal("Incorrect email or password. Please try again.")
+    } else if (response.status === 500) {
+        show_failure_modal("Oops. Looks like something went wrong on our end. Please try that again.")
+    } else if (response.status === 422) {
+        show_failure_modal("Your request was malformed and unable to be processed.")
     }
 }
 
 function register_event_handlers() {
-    document.getElementById("login-form")!.addEventListener("submit", () => { attempt_login(); return false });
+    document.getElementById("login-form")!.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        attempt_login();
+        return false // this seems to stop the form reloading with preventDefault
+    });
 }
 
 document.addEventListener('DOMContentLoaded', register_event_handlers);
