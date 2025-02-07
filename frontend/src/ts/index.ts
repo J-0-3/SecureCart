c5eae2dea443f2ff4d2a715629aac7a437f8ed26
Joe <38348883+J-0-3@users.noreply.github.com>
2025-02-07 16:12:26 +0000

    fix: updates frontend index.ts to check against /auth/check endpoints rather than /auth/whoami

--- frontend/src/ts/index.ts
+++ frontend/src/ts/index.ts
@@ -1,33 +1,47 @@
-type UserRole = "Customer" | "Administrator";
+async function check_customer(): Promise<number> {
+    const response = await fetch("/api/auth/check/customer");
+    return response.status
+}
 
-interface WhoamiResponse {
-    user_id: number,
-    role: UserRole
+async function check_admin(): Promise<number> {
+    const response = await fetch("/api/auth/check/admin");
+    return response.status
 }
 
 async function check_auth_and_redirect() {
-    const response = await fetch("/api/auth/whoami");
     const redirect_message = document.getElementById("redirect-message")!;
     const redirect_link = <HTMLAnchorElement>document.getElementById("redirect-link")!;
-    if (response.status == 200) {
-        const body: WhoamiResponse = await response.json();
-        if (body.role == "Administrator") {
+    const admin_response = await check_admin();
+    switch (admin_response) {
+        case 200:
             redirect_link.href = "/admin/dashboard.html";
             redirect_message.hidden = false;
             window.location.replace("/admin/dashboard.html");
-        } else {
+            return;
+        case 401:
+            break;
+        default:
+            redirect_link.href = `/error/${admin_response}.html`;
+            redirect_message.hidden = false;
+            window.location.replace(`/error/${admin_response}.html`);
+            return;
+    }
+    const customer_response = await check_customer();
+    switch (customer_response) {
+        case 200:
             redirect_link.href = "/store.html";
             redirect_message.hidden = false;
             window.location.replace("/store.html");
-        }
-    } else if (response.status == 401) {
-        redirect_link.href = "/login.html";
-        redirect_message.hidden = false;
-        window.location.replace("/login.html");
-    } else {
-        redirect_link.href = `/error${response.status}.html`;
-        redirect_message.hidden = false;
-        window.location.replace(`/error/${response.status}.html`)
+            break;
+        case 401:
+            redirect_link.href = "/login.html";
+            redirect_message.hidden = false;
+            window.location.replace("/login.html");
+            break;
+        default:
+            redirect_link.href = `/error/${customer_response}.html`;
+            redirect_message.hidden = false;
+            window.location.replace(`/error/${customer_response}.html`);
     }
 }
 
