dccef48ab733e9f205f7e8202a8bb0673e1c677b
Joe <38348883+J-0-3@users.noreply.github.com>
2025-02-16 00:52:56 +0000

    feat: adds status field to apporder table, makes foreign keys ON DELETE CASCADE

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TYPE app_user_role AS ENUM ('Customer', 'Administrator');
+CREATE TYPE app_order_status AS ENUM ('Unpaid', 'Confirmed', 'Fulfilled');
 
 CREATE TABLE appuser (
     id BIGSERIAL PRIMARY KEY,
@@ -13,7 +14,7 @@ CREATE TABLE appuser (
 CREATE TABLE password (
     user_id BIGINT PRIMARY KEY,
     password TEXT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES appuser(id)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES appuser(id) ON DELETE CASCADE
 );
 CREATE TABLE totp (
     user_id BIGINT PRIMARY KEY,
@@ -30,20 +31,21 @@ CREATE TABLE product_image (
     product_id BIGINT NOT NULL,
     path TEXT NOT NULL,
     PRIMARY KEY(product_id, path),
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
 );
 CREATE TABLE apporder (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL,
     order_placed TIMESTAMP NOT NULL,
     amount_charged BIGINT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES appuser(id)
+    status app_order_status NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES appuser(id) ON DELETE CASCADE
 );
 CREATE TABLE order_item(
     order_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    count BIGINT,
+    count BIGINT NOT NULL,
     PRIMARY KEY (order_id, product_id),
-    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES apporder(id),
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES apporder(id) ON DELETE CASCADE, 
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
 );
